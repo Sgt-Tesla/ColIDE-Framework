b186ef27c6982cd4d778359db91571ab02d72722
A <ahftesla@outlook.com>
2018-03-01 19:22:05 -0500

    Added a terrible user class that will probably be removed.
    Do I commit too much? Why is Message capitalized? Are their pointers in
    java?

---
+++ ColIDE-Framework/src/com/ColIDE/EndUser/User.java
@@ -0,0 +1,18 @@
+package com.ColIDE.EndUser;
+
+
+public class User 
+{
+	/**
+	 * The object to be used for the Local Code
+	 */
+	protected Object Local;
+	/**
+	 * The Object to be used as the Confirmed/Executed code
+	 */
+	protected Object Confirmed;
+	/**
+	 * The object to be used as the unconfirmed / display code
+	 */
+	protected Object Unconfirmed;
+}
